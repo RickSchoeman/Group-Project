f888be25ae87a00059ab7d7cbedfb3a70d2c1df2
Rick <rickschoeman@live.nl>
2016-04-01 15:49:04 +0200

    Klas SIE-V1A

--- src/groupProject/Klas.java
+++ src/groupProject/Klas.java
@@ -5,7 +5,6 @@ import java.io.*;
 
 public class Klas {
 	private String klasCode;
-	private ArrayList<Leerling> alleLeerlingen;
 	private ArrayList<Leerling> deLeerlingen;
 	
 	public Klas(String kC) {
@@ -16,38 +15,7 @@ public class Klas {
 		return klasCode;
 	}
 	
-	public void vulKlas()throws IOException, FileNotFoundException{
+	public void vulKlas(){
 		
-		FileReader fr = new FileReader("src/SIE_V1A.txt");
-		BufferedReader br = new BufferedReader(fr);
-		String regel = br.readLine();
-		while (regel != null) {
-			String[] values = regel.split(",");
-			String lnr = values[0] + " ";
-			String gbNm = values[3]+ " " + values[2] + " " + values[1];
-			String k = " " + values[4];
-			Leerling s = new Leerling(lnr, gbNm, k);
-			deLeerlingen.add(s);
-			System.out.printf("%s %s %s %n", lnr, gbNm, k);
-			regel = br.readLine();
-			
-		}
-		br.close();
-		
-	}
-	
-	@Override
-	public String toString() {
-		String s = klasCode+ " :\n";
-		if(deLeerlingen != null){
-			for(int i = 0; i < deLeerlingen.size(); i++) {   
-			    s = s + deLeerlingen.get(i) + "\n";
-			}  
-		}
-		else{
-			s = s + "De arrayList geeft null.";
-		}
-		
-		return s;
 	}
 }
--- src/groupProject/Main.java
+++ src/groupProject/Main.java
@@ -1,14 +1,41 @@
 package groupProject;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException{
+	public static void main(String[] args) throws IOException, FileNotFoundException {
+		ArrayList<Leerling> leerlingen= new ArrayList<Leerling>();
 		Klas k1 = new Klas("SIE_V1A");
-		k1.vulKlas();
-		System.out.println(k1.toString());
+		Klas k2 = new Klas("SIE_V1B");
+		Klas k3 = new Klas("SIE_V1C");
+		Klas k4 = new Klas("SIE_V1D");
+		Klas k5 = new Klas("SIE_V1E");
+		Klas k6 = new Klas("SIE_V1F");
+		
+		
+		FileReader fr = new FileReader("src/"+k1.getKlasCode()+".txt");
+		BufferedReader br = new BufferedReader(fr);
+		String regel = br.readLine();
+		while (regel != null) {
+			String[] values = regel.split(",");
+			String lnr = values[0] + " ";
+			String gbNm = values[3]+ " " + values[2] + " " + values[1];
+			String k = " " +k1.getKlasCode();
+			Leerling s = new Leerling(lnr, gbNm, k);
+			leerlingen.add(s);
+
+			regel = br.readLine();
+		}
+		br.close();
+		String s = k1.getKlasCode() + " bevat de volgende leerlingen: \n";
+		for(int i = 0; i < leerlingen.size(); i++) {   
+		    s = s + leerlingen.get(i) + "\n";
+		}  
+		System.out.println(s);
+		
+		
 	}
 	
 }
