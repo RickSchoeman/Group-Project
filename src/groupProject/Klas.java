9fe8a9c23f123c741e267cfe38b0fe91ca475f19
Rick <rickschoeman@live.nl>
2016-04-01 15:24:49 +0200

    mk

--- src/groupProject/Klas.java
+++ src/groupProject/Klas.java
@@ -1,12 +1,11 @@
 package groupProject;
 
 import java.util.ArrayList;
-
-
 import java.io.*;
 
 public class Klas {
 	private String klasCode;
+	private ArrayList<Leerling> alleLeerlingen;
 	private ArrayList<Leerling> deLeerlingen;
 	
 	public Klas(String kC) {
@@ -16,8 +15,6 @@ public class Klas {
 	public String getKlasCode() {
 		return klasCode;
 	}
-	
-	
 	
 	public void vulKlas()throws IOException, FileNotFoundException{
 		
@@ -29,8 +26,13 @@ public class Klas {
 			String lnr = values[0] + " ";
 			String gbNm = values[3]+ " " + values[2] + " " + values[1];
 			String k = " " + values[4];
+			Leerling s = new Leerling(lnr, gbNm, k);
+			alleLeerlingen.add(s);
+			System.out.printf("%s %s %s %n", lnr, gbNm, k);
+			regel = br.readLine();
 			
-			FileReader fr2 = new FileReader("src/"+ klasCode +".txt");
+			
+			FileReader fr2 = new FileReader("src/SIE_V1A.txt");
 			BufferedReader br2 = new BufferedReader(fr);
 			String regel2 = br.readLine();
 			while(regel2 != null){
@@ -38,34 +40,24 @@ public class Klas {
 				String lnr2 = values[0] + " ";
 				String gbNm2 = values[3]+ " " + values[2] + " " + values[1];
 				String k2 = " " + values[4];
-				if(k.equals(k2)){
-					Leerling s = new Leerling(lnr, gbNm, k);
-					deLeerlingen.add(s);
-					System.out.printf("%s %s %s %n", lnr, gbNm, k);
+				if(alleLeerlingen.contains(lnr2)){
+					Leerling s2 = new Leerling(lnr2, gbNm2, k2);
+					deLeerlingen.add(s2);
+					System.out.printf("%s %s %s %n", lnr2, gbNm2, k2);
 					regel = br.readLine();
 				}
-			}}
-			
-			
-//			Leerling s = new Leerling(lnr, gbNm, k);
-//			deLeerlingen.add(s);
-//			System.out.printf("%s %s %s %n", lnr, gbNm, k);
-//			regel = br.readLine();
-//		}
+			}
+		}
+		br.close();
+		
 	}
 	
 	@Override
 	public String toString() {
 		String s = "";
-		if(deLeerlingen != null){
-			for(int i = 0; i < deLeerlingen.size(); i++) {   
-			    s = s + deLeerlingen.get(i) + "\n";
-			}  
-		}
-		else{
-			s = s + "Het bestand geeft null.";
-		}
-		
+		for(int i = 0; i < deLeerlingen.size(); i++) {   
+		    s = s + deLeerlingen.get(i) + "\n";
+		}  
 		return s;
 	}
 }
