d86e169ec6530ffedd59aa9536b34e430318fb85
Rick <rickschoeman@live.nl>
2016-04-01 15:35:02 +0200

    nieuw

--- src/groupProject/Klas.java
+++ src/groupProject/Klas.java
@@ -18,7 +18,7 @@ public class Klas {
 	
 	public void vulKlas()throws IOException, FileNotFoundException{
 		
-		FileReader fr = new FileReader("src/klassen.txt");
+		FileReader fr = new FileReader("src/SIE_V1A.txt");
 		BufferedReader br = new BufferedReader(fr);
 		String regel = br.readLine();
 		while (regel != null) {
@@ -27,26 +27,10 @@ public class Klas {
 			String gbNm = values[3]+ " " + values[2] + " " + values[1];
 			String k = " " + values[4];
 			Leerling s = new Leerling(lnr, gbNm, k);
-			alleLeerlingen.add(s);
+			deLeerlingen.add(s);
 			System.out.printf("%s %s %s %n", lnr, gbNm, k);
 			regel = br.readLine();
 			
-			
-			FileReader fr2 = new FileReader("src/SIE_V1A.txt");
-			BufferedReader br2 = new BufferedReader(fr);
-			String regel2 = br.readLine();
-			while(regel2 != null){
-				String[] values2 = regel.split(",");
-				String lnr2 = values[0] + " ";
-				String gbNm2 = values[3]+ " " + values[2] + " " + values[1];
-				String k2 = " " + values[4];
-				if(alleLeerlingen.contains(lnr2)){
-					Leerling s2 = new Leerling(lnr2, gbNm2, k2);
-					deLeerlingen.add(s2);
-					System.out.printf("%s %s %s %n", lnr2, gbNm2, k2);
-					regel = br.readLine();
-				}
-			}
 		}
 		br.close();
 		
@@ -54,10 +38,16 @@ public class Klas {
 	
 	@Override
 	public String toString() {
-		String s = "";
-		for(int i = 0; i < deLeerlingen.size(); i++) {   
-		    s = s + deLeerlingen.get(i) + "\n";
-		}  
+		String s = klasCode+ " :\n";
+		if(deLeerlingen != null){
+			for(int i = 0; i < deLeerlingen.size(); i++) {   
+			    s = s + deLeerlingen.get(i) + "\n";
+			}  
+		}
+		else{
+			s = s + "De arrayList geeft null.";
+		}
+		
 		return s;
 	}
 }
--- src/groupProject/Main.java
+++ src/groupProject/Main.java
@@ -1,11 +1,13 @@
 package groupProject;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException, IOException{
 		Klas k1 = new Klas("SIE_V1A");
-		
+		k1.vulKlas();
 		System.out.println(k1.toString());
 	}
 	
