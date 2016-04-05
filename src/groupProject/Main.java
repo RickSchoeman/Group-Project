28b8ad4e935855abd6de593b2bc70cf123b8c4d8
Jochem <jochem.lentz@hotmail.com>
2016-04-05 12:18:36 +0200

    x
    x

--- src/groupProject/Main.java
+++ src/groupProject/Main.java
@@ -93,8 +93,9 @@ public class Main{
 		System.out.println(logL1.checkLogin());
 		
 		p1.meldPresentie();
-		if (a1.alAfwezig() == true)
+		if (a1.alAfwezig() == true){
 			a1.meldAfwezigheid();
+		}
 		if(b1.checkMelding() == true && b1.alBeter() == true){
 			b1.meldBeter();
 		}
